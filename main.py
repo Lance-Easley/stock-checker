703b3765be4ba3fcda958d60691ee739ef67cda1
Lance Easley <leasley21@basecampcodingacademy.org>
2021-01-11 11:57:56 -0600

    Added input feilds and support for 3 main stores (Amazon, Best Buy, and Newegg)

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ import colorama
 
 colorama.init()
 
-price_limit = 559.99
-item_link = "https://www.bestbuy.com/site/asus-tuf-rtx3070-8gb-gddr6-pci-express-4-0-graphics-card-black/6439128.p?skuId=6439128"
+item_link = input("Paste Item Link: ")
+price_limit = float(input("Set Budget Amount: "))
+refresh_interval = int(input("Set Refresh Interval (seconds): "))
+
+store = item_link.split('.')[1]
 
 # "https://www.bestbuy.com/site/dynex-dynex-47-75-full-motion-tv-wall-mount-black/6085403.p?skuId=6085403" testing purposes
 
@@ -17,31 +20,100 @@ PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 driver.get(item_link)
 
-try:
+if store == "bestbuy":
+    try:
+        while True:
+            element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "fulfillment-add-to-cart-button")))
+            status = element.find_element_by_class_name("btn")
+            if status.text == "Add to Cart":
+                price_element = WebDriverWait(driver, 1).until(
+                    EC.presence_of_element_located((By.CLASS_NAME, "priceView-hero-price")))
+                price = price_element.find_element_by_tag_name('span')
+                color = colorama.Fore.GREEN
+                if float(price.text[1:]) > price_limit:
+                    color = colorama.Fore.YELLOW
+                    print(colorama.Fore.WHITE + f'[{datetime.now().strftime("%H:%M:%S")}]', end=" ")
+                    print(color + "IN STOCK - Out of Budget", price.text)
+                else:
+                    print(colorama.Fore.WHITE + f'[{datetime.now().strftime("%H:%M:%S")}]', end=" ")
+                    print(color + "IN STOCK", price.text)
+                    print(item_link)
+            else:
+                print(colorama.Fore.WHITE + f'[{datetime.now().strftime("%H:%M:%S")}]', end=" ")
+                print(colorama.Fore.RED + status.text)
+            driver.refresh()
+            sleep(refresh_interval)
+    except:
+        driver.quit()
+elif store == "newegg":
+    try:
+        while True:
+            element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.ID, "ProductBuy")))
+            status = element.find_element_by_class_name("btn")
+            if status.text == "Add to Cart":
+                price = WebDriverWait(driver, 1).until(
+                    EC.presence_of_element_located((By.CLASS_NAME, "product-price")))
+                color = colorama.Fore.GREEN
+                if float(price.text[1:]) > price_limit:
+                    color = colorama.Fore.YELLOW
+                    print(colorama.Fore.WHITE + f'[{datetime.now().strftime("%H:%M:%S")}]', end=" ")
+                    print(color + "IN STOCK - Out of Budget", price.text)
+                else:
+                    print(colorama.Fore.WHITE + f'[{datetime.now().strftime("%H:%M:%S")}]', end=" ")
+                    print(color + "IN STOCK", price.text)
+                    print(item_link)
+            else:
+                print(colorama.Fore.WHITE + f'[{datetime.now().strftime("%H:%M:%S")}]', end=" ")
+                print(colorama.Fore.RED + status.text)
+            driver.refresh()
+            sleep(refresh_interval)
+    except:
+        driver.quit()
+elif store == "amazon":
+
     while True:
         element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "fulfillment-add-to-cart-button")))
-        status = element.find_element_by_class_name("btn")
-        if status.text == "Add to Cart":
-            price_element = WebDriverWait(driver, 1).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "priceView-hero-price")))
-            price = price_element.find_element_by_tag_name('span')
+            EC.presence_of_element_located((By.ID, "availability")))
+        status = element.find_element_by_class_name("a-color-success")
+
+        if status.text == "In Stock.":
+            price = WebDriverWait(driver, 1).until(
+                EC.presence_of_element_located((By.ID, "price_inside_buybox")))
             color = colorama.Fore.GREEN
-            if float(price.text[1:]) >= price_limit:
+            if float(price.text[1:]) > price_limit:
                 color = colorama.Fore.YELLOW
                 print(colorama.Fore.WHITE + f'[{datetime.now().strftime("%H:%M:%S")}]', end=" ")
-                print(color + "IN STOCK", price.text)
-                print(item_link)
+                print(color + "IN STOCK - Out of Budget", price.text)
             else:
                 print(colorama.Fore.WHITE + f'[{datetime.now().strftime("%H:%M:%S")}]', end=" ")
                 print(color + "IN STOCK", price.text)
                 print(item_link)
+
+        elif status.text == "Available from these sellers.":
+            button = status.find_element_by_tag_name('a')
+            button.click()
+            sellers = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.ID, "aod-price-1")))
+            seller_price = sellers.find_element_by_class_name('a-price-whole')
+            seller_price_fraction = sellers.find_element_by_class_name('a-price-fraction')
+            color = colorama.Fore.GREEN
+            if float(seller_price.text.replace(',', '') + '.' + seller_price_fraction.text) > price_limit:
+                color = colorama.Fore.YELLOW
+                print(colorama.Fore.WHITE + f'[{datetime.now().strftime("%H:%M:%S")}]', end=" ")
+                print(color + "IN STOCK - Out of Budget", seller_price.text + '.' + seller_price_fraction.text)
+            else:
+                print(colorama.Fore.WHITE + f'[{datetime.now().strftime("%H:%M:%S")}]', end=" ")
+                print(color + "IN STOCK - Other Seller", seller_price.text + '.' + seller_price_fraction.text)
+                print(item_link)
+
         else:
             print(colorama.Fore.WHITE + f'[{datetime.now().strftime("%H:%M:%S")}]', end=" ")
             print(colorama.Fore.RED + status.text)
         driver.refresh()
-        sleep(5)
-except:
+        sleep(refresh_interval)
+
     driver.quit()
 
 driver.quit()
